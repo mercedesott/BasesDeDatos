7af48bcb1d47abdfaa2f4fe9fea7f94502e8923c
Mercedes Ott <mermariaott@yahoo.com>
2012-12-10 19:05:53 -0200

    Bases de datos prontas
    scripts para crear pero estan vacias

---
+++ appcake.sql
@@ -0,0 +1,347 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 10, 2012 at 07:02 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `appcake`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aisles`
+--
+
+CREATE TABLE IF NOT EXISTS `aisles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `aisles`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barcodes`
+--
+
+CREATE TABLE IF NOT EXISTS `barcodes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `barcodes`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branches`
+--
+
+CREATE TABLE IF NOT EXISTS `branches` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `branches`
+--
+
+INSERT INTO `branches` (`id`, `name`) VALUES
+(1, 'Montevideo Shopping');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE IF NOT EXISTS `brands` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `brands`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE IF NOT EXISTS `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `link` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `images`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `labels`
+--
+
+CREATE TABLE IF NOT EXISTS `labels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `position_id` int(11) NOT NULL,
+  `shelf_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `aisle_id` int(11) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `number` int(11) NOT NULL,
+  `alive` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Posicion_idPosicion` (`position_id`,`shelf_id`,`product_id`,`aisle_id`),
+  KEY `shelf_id` (`shelf_id`),
+  KEY `product_id` (`product_id`),
+  KEY `aisle_id` (`aisle_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `labels`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mailing`
+--
+
+CREATE TABLE IF NOT EXISTS `mailing` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `branch_id` int(11) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Sucursal_idSucursal` (`branch_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `mailing`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `measures`
+--
+
+CREATE TABLE IF NOT EXISTS `measures` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `measures`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `positions`
+--
+
+CREATE TABLE IF NOT EXISTS `positions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `positions`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `measure_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `image_id` int(11) NOT NULL,
+  `barcode_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `quantity` double NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `featured` tinyint(1) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Medida_idMedida` (`measure_id`,`brand_id`,`image_id`,`barcode_id`),
+  KEY `brand_id` (`brand_id`),
+  KEY `image_id` (`image_id`),
+  KEY `barcode_id` (`barcode_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `products`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promotions`
+--
+
+CREATE TABLE IF NOT EXISTS `promotions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `start_date` date NOT NULL,
+  `finish_date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `finish_time` time NOT NULL,
+  `description` varchar(20) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Producto_idProducto` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `promotions`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shelves`
+--
+
+CREATE TABLE IF NOT EXISTS `shelves` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `shelves`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_type_id` int(11) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `password` char(10) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_type_id` (`user_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `users`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_types`
+--
+
+CREATE TABLE IF NOT EXISTS `user_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `user_types`
+--
+
+INSERT INTO `user_types` (`id`, `name`) VALUES
+(1, 'Administrador'),
+(2, 'Encargado de compras'),
+(3, 'Encargado de marketing'),
+(4, 'Reponedor');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `labels`
+--
+ALTER TABLE `labels`
+  ADD CONSTRAINT `labels_ibfk_1` FOREIGN KEY (`position_id`) REFERENCES `positions` (`id`),
+  ADD CONSTRAINT `labels_ibfk_2` FOREIGN KEY (`shelf_id`) REFERENCES `shelves` (`id`),
+  ADD CONSTRAINT `labels_ibfk_3` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `labels_ibfk_4` FOREIGN KEY (`aisle_id`) REFERENCES `aisles` (`id`);
+
+--
+-- Constraints for table `mailing`
+--
+ALTER TABLE `mailing`
+  ADD CONSTRAINT `mailing_ibfk_2` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`measure_id`) REFERENCES `measures` (`id`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
+  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`),
+  ADD CONSTRAINT `products_ibfk_4` FOREIGN KEY (`barcode_id`) REFERENCES `barcodes` (`id`);
+
+--
+-- Constraints for table `promotions`
+--
+ALTER TABLE `promotions`
+  ADD CONSTRAINT `promotions_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_type_id`) REFERENCES `user_types` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ supercake.sql
@@ -0,0 +1,242 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 10, 2012 at 07:05 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `supercake`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aisles`
+--
+
+CREATE TABLE IF NOT EXISTS `aisles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `aisles`
+--
+
+INSERT INTO `aisles` (`id`, `description`) VALUES
+(1, '1A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aisles_products`
+--
+
+CREATE TABLE IF NOT EXISTS `aisles_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `aisle_id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL,
+  `shelf_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `producto_id` (`product_id`,`aisle_id`,`position_id`,`shelf_id`),
+  KEY `aisle_id` (`aisle_id`),
+  KEY `position_id` (`position_id`),
+  KEY `shelf_id` (`shelf_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `aisles_products`
+--
+
+INSERT INTO `aisles_products` (`id`, `product_id`, `aisle_id`, `position_id`, `shelf_id`) VALUES
+(2, 1, 1, 1, 1),
+(3, 2, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barcodes`
+--
+
+CREATE TABLE IF NOT EXISTS `barcodes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `barcodes`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE IF NOT EXISTS `brands` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`id`, `name`) VALUES
+(1, 'Saman');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE IF NOT EXISTS `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `link` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `link`) VALUES
+(1, 'bdsihgso'),
+(2, '/product_images/4253780_700b.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `measures`
+--
+
+CREATE TABLE IF NOT EXISTS `measures` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `measures`
+--
+
+INSERT INTO `measures` (`id`, `type`) VALUES
+(1, 'kilo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `positions`
+--
+
+CREATE TABLE IF NOT EXISTS `positions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `positions`
+--
+
+INSERT INTO `positions` (`id`, `description`) VALUES
+(1, 'centro');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `measure_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `image_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `quantity` double NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `featured` tinyint(1) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `brand_id` (`brand_id`),
+  KEY `image_id` (`image_id`),
+  KEY `measure_id_brand_id_image_id` (`measure_id`,`brand_id`,`image_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `measure_id`, `brand_id`, `image_id`, `name`, `quantity`, `description`, `featured`, `price`) VALUES
+(1, 1, 1, 1, 'Arroz blanco', 1, 'Parboiled', 1, 50),
+(2, 1, 1, 1, 'Saman Parboiled', 2, 'esto es re magico', 0, 80);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shelves`
+--
+
+CREATE TABLE IF NOT EXISTS `shelves` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `shelves`
+--
+
+INSERT INTO `shelves` (`id`, `description`) VALUES
+(1, 'Primero');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `aisles_products`
+--
+ALTER TABLE `aisles_products`
+  ADD CONSTRAINT `aisles_products_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `aisles_products_ibfk_2` FOREIGN KEY (`aisle_id`) REFERENCES `aisles` (`id`),
+  ADD CONSTRAINT `aisles_products_ibfk_3` FOREIGN KEY (`position_id`) REFERENCES `positions` (`id`),
+  ADD CONSTRAINT `aisles_products_ibfk_4` FOREIGN KEY (`shelf_id`) REFERENCES `shelves` (`id`);
+
+--
+-- Constraints for table `barcodes`
+--
+ALTER TABLE `barcodes`
+  ADD CONSTRAINT `barcodes_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`measure_id`) REFERENCES `measures` (`id`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
+  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
